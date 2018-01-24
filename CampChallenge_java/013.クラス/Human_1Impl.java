14b648b19b1b0108fc767e4fb55cc62e6ce27028
ibukakentaro-PC\ibuka kentaro <sky.blue.ki.3@gmail.com>
2018-01-24 19:10:58 +0900

    クラスの継承課題提出します

---
+++ CampChallenge_java/013.クラス/Human_1Impl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+ class Human_1Impl extends Human_1 {
+
+    public void clear() {
+        this.name=null;
+       this.birth =0;    
+    }
+}
