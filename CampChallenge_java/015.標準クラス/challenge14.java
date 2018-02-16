177eb9c672c56364df8d84a178a0f8ba026953d4
ibukakentaro-PC\ibuka kentaro <sky.blue.ki.3@gmail.com>
2018-02-16 11:53:27 +0900

    バイト数と文字数の取得課題提出します

---
+++ CampChallenge_java/015.標準クラス/challenge14.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+/**
+ *
+ * @author ibuka kentaro
+ */
+public class challenge14 {
+
+    public static void main(String[] args) {
+        String word = "井深健太郎";
+
+        System.out.println(word.getBytes().length);
+        System.out.println(word.length());
+    }
+
+}
