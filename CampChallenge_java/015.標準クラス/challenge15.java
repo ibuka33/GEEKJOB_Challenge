bacb87faf364d906628e006b7506ee0b0046a1df
ibukakentaro-PC\ibuka kentaro <sky.blue.ki.3@gmail.com>
2018-02-19 10:53:47 +0900

    文字数の取得課題提出します

---
+++ CampChallenge_java/015.標準クラス/challenge15.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+/**
+ *
+ * @author ibuka kentaro
+ */
+public class challenge15 {
+
+    public static void main(String[] args) {
+        String address = "GEEK-JOB-kadai@gmail.com";
+
+        System.out.println(address.substring(15));
+
+    }
+}
